40d2e783dcf9a35ddd8045496e7a799ac8b6cbd1
张文川 <13522750636@163.com>
2022-03-18 16:48:48 +0800

    feat: 搭建项目基本框架

---
+++ src/application/Rank/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Home(props) {
+  return (
+    <div>Rank</div>
+  )
+}
+
+export default React.memo(Home)
---
+++ src/application/Singers/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Home(props) {
+  return (
+    <div>Singers</div>
+  )
+}
+
+export default React.memo(Home)
